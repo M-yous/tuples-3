93e99fd9f72f58602c72f7260b1b9920d5a5edf5
M-yous <145792550+M-yous@users.noreply.github.com>
2023-11-03 16:22:57 +0000

    Add files via upload

---
+++ using-tuples-and-sets-part-3.py
@@ -0,0 +1,27 @@
+# Import required modules/packages/library
+from pprint import pprint
+# creat the outer list for all device
+devices = []
+# Open the file and resd in the device info
+file = open('devices-04.txt', 'r')
+for line in file:
+    # Add device info into tuple
+    device_info = tuple(line.strip().split(','))
+
+    # Display what we have read and bulit so far
+    print('Read line: ', device_info)
+
+    # Append our device and its info onto our 'devices' list
+    devices.append(device_info)
+
+# Display a blank line to make easier to read
+print('')
+
+
+# Display a title
+print('Input converted to a list tuples:')
+# Display the tupl with nice formatting
+pprint(devices)
+
+# Close the file
+file.close()
